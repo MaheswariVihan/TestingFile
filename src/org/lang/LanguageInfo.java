cc05700f3fdcc0c5a17a4d9a02a4793a4a18bc5f
MaheswariVihan <130825531+MaheswariVihan@users.noreply.github.com>
2023-04-28 13:47:51 +0530

    Update LanguageInfo.java

--- src/org/lang/LanguageInfo.java
+++ src/org/lang/LanguageInfo.java
@@ -1,15 +1,7 @@
 package org.lang;
 
 public class LanguageInfo {
-	public void tamilLanguage() {
-	System.out.println("Tamil Language:70 %") ;
-	}
-	public void englishLanguage() {
-		System.out.println("English Language:90 %") ;
-		}
-	public void hindiLanguage() {
-		System.out.println("Hindi Language:60 %") ;
-		}
+	
 	
 	public static void main(String[] args) {
 		System.out.println("Languages Info");
