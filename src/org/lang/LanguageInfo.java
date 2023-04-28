9d353632241b3ebc45e1e2c532ddac7f4f98ba80
M <maheswari.m@hp.com>
2023-04-28 13:45:24 +0530

    Secong commit

--- src/org/lang/LanguageInfo.java
+++ src/org/lang/LanguageInfo.java
@@ -10,8 +10,11 @@ public class LanguageInfo {
 	public void hindiLanguage() {
 		System.out.println("Hindi Language:60 %") ;
 		}
+	
 	public static void main(String[] args) {
-		System.out.println("Languages Info");	
+		System.out.println("Languages Info");
+		System.out.println("MahePrasanth");
+		System.out.println("MaheBijju");
 	}
 
 }
